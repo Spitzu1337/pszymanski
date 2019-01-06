d363bc8bc40e53e5c7b94d2b9f5e42180e4c9a33
Spitzu1337 <46418637+Spitzu1337@users.noreply.github.com>
2019-01-06 13:25:39 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package pl.piotrszymański.udemy.firstprogram;
+
+// to jest tekst, który nic nie zmieni
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Intellij!");
+        /*
+        to jest komentarz na kilka linii
+        np na dwie
+        lub na trzy
+         */
+    }
+}
